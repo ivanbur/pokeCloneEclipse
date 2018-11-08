f20aa6367f95036fa105610f964f2cbc2dabbf10
MV Admin <mvadmin@MVCC-MACMINI-72.local>
2018-11-07 19:33:02 -0800

    did some stuff

--- workspace/pokeClone/src/pokeClone/Player.java
+++ workspace/pokeClone/src/pokeClone/Player.java
@@ -1,5 +1,12 @@
 package pokeClone;
 
-public class Player {
-
+public class Player extends Sprite {
+	private int x;
+	private int y;
+	private int mapRow;
+	private int mapColumn;
+	
+	public Player(String filename, int x, int y, int mapRow, int mapColumn, int numberRows, int numberFrames, int sizeOfClipWidth, int sizeOfClipHeight) {
+		super(filename, numberRows, numberFrames, sizeOfClipWidth, sizeOfClipHeight);
+	}
 }
